655625657ec06cddfe8c622a78131ebe4785b557
santospatricia11 <patricia.pereira@academico.ifpb.edu.br>
2022-10-14 21:45:42 -0300

    inicio

---
+++ sacola-api/src/main/java/me/dio/sacola/model/Cliente.java
@@ -0,0 +1,23 @@
+package me.dio.sacola.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Entity
+@AllArgsConstructor
+@Builder
+@Data
+@JsonIgnoreProperties({"hibernateLzyInitializer","handler"})
+@NoArgsConstructor
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @Embedded
+    private  Endereco endereco;
+}
