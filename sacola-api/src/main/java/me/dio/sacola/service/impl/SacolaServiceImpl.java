c49c93c3fafa2c29d5ae1292fe09def6185c5d88
santospatricia11 <patricia.pereira@academico.ifpb.edu.br>
2022-10-16 22:26:02 -0300

    atualizações

--- sacola-api/src/main/java/me/dio/sacola/service/impl/SacolaServiceImpl.java
+++ sacola-api/src/main/java/me/dio/sacola/service/impl/SacolaServiceImpl.java
@@ -12,6 +12,7 @@ import me.dio.sacola.resource.dto.ItemDTO;
 import me.dio.sacola.service.SacolaService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -52,8 +53,18 @@ public class SacolaServiceImpl implements SacolaService {
                         "esvazie");
             }
         }
+
+        List<Double> valorDosItens = new ArrayList<>();
+        for(Item itemDaSacola:itensDaSacola){
+            double valorTotalItem = itemDaSacola.getProduto().getValorUnitario()*itemDaSacola.getQuantidade();
+            valorDosItens.add(valorTotalItem);
+        }
+        double valorTotalSacola = valorDosItens.stream()
+                .mapToDouble(valorTotalDeCadaItens -> valorTotalDeCadaItens)
+                .sum();
+        sacola.setValorTotal(valorTotalSacola);
         sacolaRepository.save(sacola);
-        return itemRepository.save(itemParaSerInserido);
+        return itemParaSerInserido;
     }
 
     @Override
